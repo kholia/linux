55f76a28c510951284db6223bfa246aff702b932
Lai Jiangshan <jiangshan.ljs@antgroup.com>
2024-01-09 19:44:41 +0800

    KVM: x86: Set 'vcpu->arch.exception.injected' as true before vendor callback
    For PVM, the exception is injected and delivered directly in the
    callback before VM enter, so it will clear
    'vcpu->arch.exception.injected'.  Therefore, if
    'vcpu->arch.exception.injected' is set to true after the vendor
    callback, it may inject the same exception repeatedly in PVM. To address
    this, move the setting of 'vcpu->arch.exception.injected' to true before
    the vendor callback in kvm_inject_exception(). This adjustment has no
    influence on VMX/SVM, as they don't change it in their callbacks.
    
    No functional change.
    
    Signed-off-by: Lai Jiangshan <jiangshan.ljs@antgroup.com>
    Signed-off-by: Hou Wenlong <houwenlong.hwl@antgroup.com>

--- arch/x86/kvm/x86.c
+++ arch/x86/kvm/x86.c
@@ -10588,6 +10588,7 @@ static void kvm_inject_exception(struct kvm_vcpu *vcpu)
 				vcpu->arch.exception.error_code,
 				vcpu->arch.exception.injected);
 
+	vcpu->arch.exception.injected = true;
 	kvm_x86_call(inject_exception)(vcpu);
 }
 
@@ -10737,7 +10738,6 @@ static int kvm_check_and_inject_events(struct kvm_vcpu *vcpu,
 		kvm_inject_exception(vcpu);
 
 		vcpu->arch.exception.pending = false;
-		vcpu->arch.exception.injected = true;
 
 		can_inject = false;
 	}
