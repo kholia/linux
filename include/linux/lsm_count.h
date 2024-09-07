71c9e83885050f687bb188040e1cf156551a4afa
Dhiru Kholia <dhiru.kholia@gmail.com>
2024-09-07 21:10:24 +0530

    My Test LSM

--- include/linux/lsm_count.h
+++ include/linux/lsm_count.h
@@ -102,6 +102,12 @@
 #define IPE_ENABLED
 #endif
 
+#if IS_ENABLED(CONFIG_SECURITY_MY_TEST)
+#define MY_TEST_ENABLED 1,
+#else
+#define MY_TEST_ENABLED
+#endif
+
 /*
  *  There is a trailing comma that we need to be accounted for. This is done by
  *  using a skipped argument in __COUNT_LSMS
@@ -124,7 +130,8 @@
 		LANDLOCK_ENABLED	\
 		IMA_ENABLED		\
 		EVM_ENABLED		\
-		IPE_ENABLED)
+		IPE_ENABLED		\
+		MY_TEST_ENABLED)
 
 #else
 
